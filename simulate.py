3b8458c86039348ef98870968467f9ca734384de
“Hanif <2906978@student.gla.ac.uk>
2026-01-25 12:13:52 -0500

    instruction 10

--- simulate.py
+++ simulate.py
@@ -2,8 +2,8 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
-for i in range(100):
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+for i in range(1000):
     time.sleep(1/60)
     print(i)
     p.stepSimulation()
