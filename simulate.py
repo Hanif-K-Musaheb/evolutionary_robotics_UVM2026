234192cb86dc3937eb1bf5e505ebac752b3934af
“Hanif <2906978@student.gla.ac.uk>
2026-01-25 13:13:35 -0500

    instruction 29

--- simulate.py
+++ simulate.py
@@ -7,7 +7,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")# the floor of the simulation
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 for i in range(1000):
     time.sleep(1/60)
     print(i)
