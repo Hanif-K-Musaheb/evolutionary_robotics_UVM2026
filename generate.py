a994fb58f9607bf250e0bbd078e904115a266056
“Hanif <2906978@student.gla.ac.uk>
2026-01-31 20:14:57 -0500

    instructoin 27

--- generate.py
+++ generate.py
@@ -10,7 +10,16 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     x, y, z = 0, 0, 0.5
+    legX,legY,legZ= 1,0,1.5
     pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[1, 1, 1])
+    pyrosim.Send_Joint( name = "Torso_Leg" ,
+                        parent= "Torso" ,
+                        child = "Leg" ,
+                        type = "revolute",
+                        position = [0.5,0,1])
+    pyrosim.Send_Cube(name="Leg", pos=[legX,legY,legZ], size=[1, 1, 1])
+
+    
     pyrosim.End()
 
 
