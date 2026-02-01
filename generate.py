7679d18098b2ad0c568d08e8cebe06407cd7fc79
“Hanif <2906978@student.gla.ac.uk>
2026-01-31 19:20:06 -0500

    instruction 8

--- generate.py
+++ generate.py
@@ -2,9 +2,7 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("boxes.sdf")
 x,y,z = 0,0,.5
-x2,y2,z2=1,0,1.5
 pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[1,1,1])
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2], size=[1,1,1])
 
 
 
