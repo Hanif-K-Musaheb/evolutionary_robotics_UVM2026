b29c420cad75e50415d0a297d80ebf06ef840c36
“Hanif <2906978@student.gla.ac.uk>
2026-01-31 19:49:36 -0500

    instruction 20

--- generate.py
+++ generate.py
@@ -1,9 +1,18 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
-x,y,z = 0,0,.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[1,1,1])
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    x,y,z = -2,0,.5
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[1,1,1])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    x, y, z = 0, 0, 0.5
+    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[1, 1, 1])
+    pyrosim.End()
 
 
-
-pyrosim.End()
+Create_World()
+Create_Robot()
