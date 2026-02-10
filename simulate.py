21d582c4586b3afae9e60eaae14e95d8103be43f
“Hanif <2906978@student.gla.ac.uk>
2026-02-10 12:12:29 -0500

    instruction 8

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import pybullet_data
 import time
+import pyrosim.pyrosim as pyrosim
+
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -9,9 +11,16 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")# the floor of the simulation
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+
+pyrosim.Prepare_To_Simulate(robotId)
+
 for i in range(1000):
-    time.sleep(1/60)
     print(i)
     p.stepSimulation()
+
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+
+    time.sleep(1/120)
+    
 
 p.disconnect()
